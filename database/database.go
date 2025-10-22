df6d86936e6f8d119a4fb6ba5365e7540d95bfdf
haraamurti <haramurti@student.ub.ac.id>
2025-10-22 22:40:04 +0700

    6.cr database.go

--- database/database.go
+++ database/database.go
@@ -1,1 +1,25 @@
 package database
+
+import (
+	"log"
+	"valeth-soundcloud-api/config" // Mengimpor paket config yang Anda buat
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// ConnectDatabase menggunakan Config struct untuk terhubung ke DB.
+func ConnectDatabase(config config.Config) (*gorm.DB, error) {
+	// Di sini kita menggunakan nilai yang sudah dibaca dari .env
+	// yaitu "db_URL" yang Anda petakan ke DatabaseURL
+	dsn := config.DatabaseURL
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Gagal terhubung ke Database")
+		return nil, err
+	}
+
+	return db, nil
+}
+
