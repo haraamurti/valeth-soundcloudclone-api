b2e2e674f48c4edcf90916f451b4eaec39437bab
haraamurti <haramurti@student.ub.ac.id>
2025-10-22 22:06:54 +0700

    notes on config file

--- config/config.go
+++ config/config.go
@@ -1,30 +1,33 @@
 package config
 
 import (
+	"fmt"
+
 	"github.com/spf13/viper"
 )
 
-// Config struct holds all configuration for the application.
-// The `mapstructure` tags tell Viper which .env key maps to which struct field.
+
+
 type Config struct {
-	DatabaseURL    string `mapstructure:"db_URL"`
-	SupabaseURL    string `mapstructure:"SUPABASE_URL"`
-	SupabaseKey    string `mapstructure:"SUPABASE_KEY"`
-	SupabaseBucket string `mapstructure:"SUPABASE_BUCKET"`
+	DatabaseURL    string `mapstructure:"db_URL"` //menset database url untuk dari supabase
+	SupabaseURL    string `mapstructure:"SUPABASE_URL"` // akan dipakai nanti
+	SupabaseKey    string `mapstructure:"SUPABASE_KEY"`//akan dipakai nanti
+	SupabaseBucket string `mapstructure:"SUPABASE_BUCKET"`//akan dipakai nanti
 }
 
-// LoadConfig reads configuration from the .env file in the specified path.
+
 func LoadConfig(path string) (config Config, err error) {
-	viper.AddConfigPath(path)   // Tell viper where to look for the file
+	viper.AddConfigPath(path)   //
 	viper.SetConfigName(".env") // The name of the config file (without extension)
 	viper.SetConfigType("env")  // The type of the file
 
-	viper.AutomaticEnv() // Also read from system environment variables if available
+	//viper.AutomaticEnv() // this is going to read the env from the os enviorment. if we have to
 
-	// Find and read the config file
+	// this will read the config file.
 	err = viper.ReadInConfig()
 	if err != nil {
 		return
+		fmt.Println("erorr reading config..")
 	}
 
 	// "Unmarshal" the loaded values into our Config struct
