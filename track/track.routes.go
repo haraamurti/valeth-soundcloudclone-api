5e587ed74423518a6812c6d98b93f81a4824a9d0
haraamurti <haramurti@student.ub.ac.id>
2025-11-04 00:43:24 +0700

    route functinya ketinggalan

--- track/track.routes.go
+++ track/track.routes.go
@@ -12,5 +12,21 @@ func SetupTrackRoutes(api fiber.Router, handler *Handler) {
 	// Daftarkan endpoint POST /api/v1/tracks/upload
 	//link unutk upload.
 	tracks.Post("/upload", handler.UploadTrack)
+	tracks.Get("/u", handler.UploadTrack)
+
+
+	// --- ⬇⬇⬇ RUTE-RUTE BARU KITA ⬇⬇⬇ ---
+
+	// RUTE 1: GET /api/v1/tracks
+	// (Mengambil daftar semua lagu sebagai JSON)
+	tracks.Get("/", handler.GetAllTracks)
+
+	// RUTE 2: GET /api/v1/tracks/1/audio
+	// (Memainkan/streaming audio dari track dengan ID 1)
+	tracks.Get("/:id/audio", handler.StreamTrack)
+
+	// RUTE 3: GET /api/v1/tracks/1/cover
+	// (Melihat/streaming gambar cover dari track dengan ID 1)
+	tracks.Get("/:id/cover", handler.StreamCover)
 
 }
